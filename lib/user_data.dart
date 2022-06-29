e478d6675cd7be7c545694cc590c6074abb50de6
FlutLab.io <>
2022-06-29 20:46:38 +0000

    Initial commit

---
+++ lib/user_data.dart
@@ -0,0 +1,8 @@
+class UserData {
+  double icilenSigara;
+  double sporGunu;
+  int boy;
+  int kilo;
+
+  UserData({required this.icilenSigara, required this.sporGunu, required this.boy, required this.kilo});
+}
